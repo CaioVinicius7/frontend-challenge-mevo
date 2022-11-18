c0ec9b5903b66af19543d6a397f52f49931922ad
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-18 16:37:00 -0300

    feat: :children_crossing: Adição de title na página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Head from "next/head";
 import { Center, Flex, Text, VStack } from "@chakra-ui/react";
 
 import { Scoreboard } from "../components/Scoreboard";
@@ -20,58 +21,63 @@ export default function Home() {
   }
 
   return (
-    <VStack maxW={820} mx="auto" mt="14" px="8">
-      <Scoreboard />
+    <>
+      <Head>
+        <title> Jokenpo | Home </title>
+      </Head>
+      <VStack maxW={820} mx="auto" mt="14" px="8">
+        <Scoreboard />
 
-      {isLoading ? (
-        <Flex w="full" flexWrap="wrap" pt="50" justifyContent="center">
-          <Center w="50%">
-            <ChoiceButton variant={playerChoice} />
-          </Center>
+        {isLoading ? (
+          <Flex w="full" flexWrap="wrap" pt="50" justifyContent="center">
+            <Center w="50%">
+              <ChoiceButton variant={playerChoice} />
+            </Center>
 
-          <Center w="50%">
-            <ChoiceButton variant="Load" />
-          </Center>
+            <Center w="50%">
+              <ChoiceButton variant="Load" />
+            </Center>
 
-          <Text
-            mt="10"
-            fontSize={["2xl", "3xl", "4xl"]}
-            fontWeight="bold"
-            color="gray.50"
-          >
-            MACHINE IS CHOOSING...
-          </Text>
-        </Flex>
-      ) : (
-        <Flex w="full" flexWrap="wrap" pt="50">
-          <Center w="50%">
-            <ChoiceButton
-              variant="Rock"
-              onClick={() => {
-                handleStartGame("Rock");
-              }}
-            />
-          </Center>
+            <Text
+              mt="10"
+              fontSize={["2xl", "3xl", "4xl"]}
+              fontWeight="bold"
+              color="gray.50"
+            >
+              MACHINE IS CHOOSING...
+            </Text>
+          </Flex>
+        ) : (
+          <Flex w="full" flexWrap="wrap" pt="50">
+            <Center w="50%">
+              <ChoiceButton
+                variant="Rock"
+                onClick={() => {
+                  handleStartGame("Rock");
+                }}
+              />
+            </Center>
 
-          <Center w="50%">
-            <ChoiceButton
-              variant="Paper"
-              onClick={() => {
-                handleStartGame("Paper");
-              }}
-            />
-          </Center>
+            <Center w="50%">
+              <ChoiceButton
+                variant="Paper"
+                onClick={() => {
+                  handleStartGame("Paper");
+                }}
+              />
+            </Center>
 
-          <Center w="full" pt={["4", "10"]}>
-            <ChoiceButton
-              variant="Scissors"
-              onClick={() => {
-                handleStartGame("Scissors");
-              }}
-            />
-          </Center>
-        </Flex>
-      )}
-    </VStack>
+            <Center w="full" pt={["4", "10"]}>
+              <ChoiceButton
+                variant="Scissors"
+                onClick={() => {
+                  handleStartGame("Scissors");
+                }}
+              />
+            </Center>
+          </Flex>
+        )}
+      </VStack>
+    </>
   );
 }
